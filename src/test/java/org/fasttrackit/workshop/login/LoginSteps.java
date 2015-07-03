4636771e1e32409b2c38de21473fea6d71b545c1
lucian.micu <lucian.micu@ebs.ro>
2015-07-03 14:51:35 +0300

    our first cucumber steps

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -1,7 +1,11 @@
 package org.fasttrackit.workshop.login;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.xpath.SourceTree;
 import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
 import org.openqa.selenium.By;
@@ -27,4 +31,51 @@ public class LoginSteps extends TestBaseNative {
         element.sendKeys(email);
     }
 
+    @Given("^I open this URL \"([^\"]*)\"$")
+    public void I_open_this_URL(String argument) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+
+    }
+
+    @Then("^I send (\\d+) into search field$")
+    public void I_send_into_search_field(int arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        System.out.println("numarul este" + arg1);
+    }
+
+    @Given("^I acces the login page$")
+    public void I_acces_the_login_page() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @And("^I insert valid credentials$")
+    public void I_insert_valid_credentials() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I click login button$")
+    public void I_click_login_button() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I check if user was logged in$")
+    public void I_check_if_user_was_logged_in() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @And("^I insert invalid credentials$")
+    public void I_insert_invalid_credentials() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I expect invalid credentials message$")
+    public void I_expect_invalid_credentials_message() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
 }
--- src/test/resources/feature/workshop/login.feature
+++ src/test/resources/feature/workshop/login.feature
@@ -1,1 +1,18 @@
+Feature: Login
 
+  Scenario: open any webpage
+    Given I open this URL "http://google.com"
+    Then I send 5 into search field
+
+  Scenario: Login succesfully
+   Given I acces the login page
+   And I insert valid credentials
+   When I click login button
+   Then I check if user was logged in
+
+
+  Scenario: login with wrong credentials
+    Given I acces the login page
+    And I insert invalid credentials
+    When I click login button
+    Then I expect invalid credentials message
