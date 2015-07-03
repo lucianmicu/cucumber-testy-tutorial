9574fc60fbcd947aa221833b644f2b020ee5b94c
lucian.micu <lucian.micu@ebs.ro>
2015-07-03 16:53:08 +0300

    our first cucumber steps

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -1,20 +1,19 @@
 package org.fasttrackit.workshop.login;
 
 import com.sdl.selenium.web.utils.Utils;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.java.en_old.Thathe;
-import org.apache.xpath.SourceTree;
 import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LoginSteps extends TestBaseNative {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginSteps.class);
@@ -72,19 +71,20 @@ public class LoginSteps extends TestBaseNative {
     @Then("^I check if user was logged in$")
     public void I_check_if_user_was_logged_in() throws Throwable {
         boolean successsfullLogin = false;
-       WebElement logout = driver.findElement(By.linkText("Logout"));
-        boolean succesLoggedIn = logout.isDisplayed();("Could not find logout button", succesLoggedIn, is(true));
+       WebElement btnlogout = driver.findElement(By.partialLinkText("Logout"));
+        successsfullLogin = btnlogout.isDisplayed();
+        assertThat(successsfullLogin, is(true));
     }
+
+
 
     @And("^I insert invalid credentials$")
     public void I_insert_invalid_credentials() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
     }
 
     @Then("^I expect invalid credentials message$")
     public void I_expect_invalid_credentials_message() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
     }
 }
